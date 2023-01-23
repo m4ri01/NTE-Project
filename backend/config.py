bf458f739556350bd4d2fc5960bdd3a2f7c55bb9
Melki Mario Gulo <melkimariogulo@gmail.com>
2023-01-23 17:20:31 +0700

    0.0.2 Docker Compose Bug Fixing

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 from urllib.parse import quote
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "mysql://root:%s@db:3306/telkom" % quote("P@ssw0rd")
+SQLALCHEMY_DATABASE_URI = "mysql://root:%s@dbService:3306/telkom" % quote("P@ssw0rd")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
