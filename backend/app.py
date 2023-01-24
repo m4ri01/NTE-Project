c8465317c17c4649c77d2e253731004c9467c8c0
Melki Mario Gulo <melkimariogulo@gmail.com>
2023-01-24 14:50:40 +0700

    0.1.0 Add CORS and migrate service in the Docker entrypoint

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS,cross_origin
 from Models.DB import db
 import Controllers
 from Routers.RouterONT import ONTBP
 app = Flask(__name__)
 app.config.from_object('config')
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 db.init_app(app)
 migrate = Migrate(app, db)
